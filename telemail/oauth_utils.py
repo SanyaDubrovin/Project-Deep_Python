d047cd876f6702187457d306b2f5ae6e401ecc63
Elizaveta <eisergienko@edu.hse.ru>
2024-12-16 19:08:26 +0300

    Removed initialising of google vars with None

--- telemail/oauth_utils.py
+++ telemail/oauth_utils.py
@@ -5,9 +5,9 @@ from dotenv import dotenv_values
 
 
 CONFIG = dotenv_values('.env')
-GOOGLE_CLIENT_ID = CONFIG.get('GOOGLE_CLIENT_ID', None)
+GOOGLE_CLIENT_ID = CONFIG.get('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = CONFIG.get('GOOGLE_CLIENT_SECRET', os.urandom(24))
-GOOGLE_DISCOVERY_URL = CONFIG.get('GOOGLE_DISCOVERY_URL', None)
+GOOGLE_DISCOVERY_URL = CONFIG.get('GOOGLE_DISCOVERY_URL')
 
 def get_google_provider_cfg():
     # Find out what URL to hit to get tokens that allow you to ask for

5a9cdd76d08b17bf29ebff4c158cfbadf75edbd0
Elizaveta <eisergienko@edu.hse.ru>
2024-12-17 12:55:53 +0300

    Added get_db func, new temp table and poll_period field

--- telemail/db.py
+++ telemail/db.py
@@ -1,5 +1,25 @@
 import psycopg2
 
+from dotenv import dotenv_values
+
+
+def get_db(
+        database: str,
+        user: str,
+        password: str,
+        host: str,
+        port: int,
+        init_tables=True
+    ):
+    config = dotenv_values('.env')
+    return TelemailDB(
+        database=config['DATABASE_NAME'],
+        user=config['DATABASE_USERNAME'],
+        password=config['DATABASE_USER_PASSWORD'],
+        host=config['DATABASE_HOST'],
+        port=config['DATABASE_PORT'],
+        init_tables=True
+    )
 
 class TelemailDB(object):
     def __init__(
@@ -8,17 +28,20 @@ class TelemailDB(object):
             user: str,
             password: str,
             host: str,
-            port: int
+            port: int,
+            init_tables=True
         ):
         self.database = database
         self.user = user
         self.password = password
         self.host = host
         self.port = port
-        self.connection = None
+        self.connection = self.connect()
+        if init_tables:
+            self.initialise_tables()
 
     def connect(self):
-        self.connection = psycopg2.connect(
+        return psycopg2.connect(
             host=self.host,
             port=self.port,
             user=self.user,
@@ -31,8 +54,22 @@ class TelemailDB(object):
             raise TypeError('Connect to database before initialising tables!')
         with self.connection.cursor() as cursor:
             cursor.execute("""
-                CREATE TEMPORARY TABLE IF NOT EXISTS telemail_tg_register_start (
+                CREATE TEMPORARY TABLE IF NOT EXISTS telemail_tg_register_temp (
                     chat_id VARCHAR(32), email VARCHAR(128)
+                )
+            """)
+            cursor.execute("""
+                CREATE TEMPORARY TABLE IF NOT EXISTS google_user_info_temp (
+                    google_unique_id VARCHAR(32),
+                    email VARCHAR(128) NOT NULL,
+                    username VARCHAR(128),
+                    verified BOOLEAN,
+                    id_token VARCHAR(2048),
+                    access_token VARCHAR(256),
+                    token_type VARCHAR(16),
+                    scope VARCHAR(256),
+                    expires_in INT,
+                    token_register_datetime DATETIME
                 )
             """)
             # token_type # str (value: 'Bearer')
@@ -41,6 +78,7 @@ class TelemailDB(object):
                     chat_id VARCHAR(32) NOT NULL,
                     google_unique_id VARCHAR(32),
                     email VARCHAR(128) NOT NULL,
+                    email_poll_period SMALLINT,
                     username VARCHAR(128),
                     verified BOOLEAN,
                     id_token VARCHAR(2048),
@@ -58,14 +96,61 @@ class TelemailDB(object):
     def insert_tg_user_temp(self, chat_id: str, email: str):
         with self.connection.cursor() as cursor:
             cursor.execute("""
-                    INSERT INTO telemail_tg_register_start (chat_id, email) VALUES (%s, %s)
+                    INSERT INTO telemail_tg_register_temp (chat_id, email) VALUES (%s, %s)
                 """,
                 (chat_id, email)
             )
-
-    def get_temp_users(self):
+    
+    def insert_google_user_temp(self,
+            google_unique_id,
+            email,
+            username,
+            verified,
+            id_token,
+            access_token,
+            token_type,
+            scope,
+            expires_in,
+            token_register_datetime,
+            email_poll_period=60
+        ):
         with self.connection.cursor() as cursor:
-            cursor.execute('SELECT * FROM telemail_tg_register_start')
+            cursor.execute("""INSERT INTO google_user_info_temp (
+                    google_unique_id,
+                    email,
+                    email_poll_period,
+                    username,
+                    verified,
+                    id_token,
+                    access_token,
+                    token_type,
+                    scope,
+                    expires_in,
+                    token_register_datetime
+                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+                    google_unique_id,
+                    email,
+                    email_poll_period,
+                    username,
+                    verified,
+                    id_token,
+                    access_token,
+                    token_type,
+                    scope,
+                    expires_in,
+                    token_register_datetime
+                )
+            )
+
+    def get_tg_temp_users(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM telemail_tg_register_temp')
+            return cursor.fetchall()
+    
+    def get_google_temp_users(self):
+        with self.connection.cursor() as cursor:
+            cursor.execute('SELECT * FROM google_user_info_temp')
             return cursor.fetchall()
 
     def insert_new_user(
@@ -80,7 +165,8 @@ class TelemailDB(object):
                 token_type,
                 scope,
                 expires_in,
-                token_register_datetime
+                token_register_datetime,
+                email_poll_period=60
             ):
         with self.connection.cursor() as cursor:
             cursor.execute("""
@@ -88,6 +174,7 @@ class TelemailDB(object):
                         chat_id,
                         google_unique_id,
                         email,
+                        email_poll_period,
                         username,
                         verified,
                         id_token,
@@ -96,7 +183,7 @@ class TelemailDB(object):
                         scope,
                         expires_in,
                         token_register_datetime
-                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                 """,
                 (
                     chat_id,
